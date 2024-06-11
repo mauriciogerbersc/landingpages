3bf6460ede380fef7370749c041b35d0b2e74a4b
mauricio3332406 <mauricio@greenn.com.br>
2024-06-11 16:58:27 -0300

    alteracoes de layout

--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -1,76 +1,68 @@
 <template>
    <body class="lp">
     <div class="page-wrapper">
-        <section class="one-page bgp-bottom bgp-bottom bgc-black py-100 rpt-95 rpb-100" :style="backgroundImageStyle">
+        <section class="one-page bgp-bottom bgp-bottom bgc-black py-50 rpt-95 rpb-100" :style="backgroundImageStyle">
             <div class="container">
                 <div class="row gap-150 justify-content-center">
-                    <div class="col-lg-6">
+                    <div class="lp-content style-two text-white">
+                        <h1>Venha trilhar comigo a jornada do <span>seu crescimento</span></h1>
+                    </div>
+
+                    <div class="col-lg-6 ">
+                        <div class="lp-form-bg">
+                            <img :src="formBg">
+                        </div>
+                    </div>
+           
+                    <div class="col-lg-6 rel z-1">
                         <div class="lp-content style-two text-white">
-                            <h1>Descubra a Jornada do Seu Crescimento como <span>Artesã Digital</span>
-                            </h1>
-                            <p>Olá, eu sou Duda, e minha jornada no mundo do artesanato começou lá em 2018. Naquela
-                                época, eu tive que aprender tudo do zero, desde as técnicas de artesanato até a
-                                construção de um negócio de sucesso nas redes sociais. Com muito esforço e dedicação, me
-                                destaquei no mercado e, em 2020, fundei a Amorino Fios. No ano seguinte, criei A Casa de
-                                Palha Acessórios, e hoje, nossas criações já alcançaram mais de 30 países ao redor do
-                                mundo.</p>
-                            <p> Agora, estou aqui para ajudar você a trilhar o mesmo caminho de sucesso. Se você quer
-                                transformar sua paixão pelo artesanato em um negócio próspero, eu te convido a
-                                participar do nosso evento de lançamento gratuito do
-                                curso "Artesãs Digitais: A Jornada do Seu Crescimento".
+                            <p>
+                                Minha história no mundo do artesanato começou em 2018, eu tive que aprender tudo do zero, desde as técnicas de artesanato até a construção de um negócio de sucesso nas redes sociais. 
                             </p>
+                            <p>Consegui me destacar nesse mercado e criar duas empresas do ramo, alcançando todos os estados brasileiros e mais de 30 países.</p>
+                            <p>Agora vou ajudar você a construir um caminho de sucesso. Se você quer transformar sua paixão pelo artesanato em um negócio próspero, eu te convido a participar do nosso evento de lançamento gratuito!</p>
+                  
+                            <div class="lp-content style-two text-white">
+                                <h4><span>Garanta seu lugar no evento de lançamento gratuito:</span></h4>
+                            </div>
+
+                            <div class="lp-form">
+
+                                <form @submit.prevent="submitForm">
+                                    <div class="form-group">
+                                        <input type="text" id="name" v-model="name" required class="form-control" value=""
+                                            placeholder="Seu nome">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="text" id="name" v-model="email" class="form-control" value=""
+                                            placeholder="Seu e-mail" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="text" id="name" v-model="phone" class="form-control" value=""
+                                            placeholder="Seu whatsapp" required>
+                                    </div>
+                                    <div class="form-group py-30 mb-0">
+                                        <button v-if="!isLoading" type="submit" class="theme-btn">QUERO PARTICIPAR <i class="fas fa-solid fa-arrow-right"></i></button>
+                                        <div v-else class="loader">Enviando...</div>
+                                    </div>
+                                </form>
+                                <div v-if="successMessage" class="success-message" v-html="successMessage"></div>
+                            </div>
 
                         </div>
                     </div>
-                    <div class="col-lg-6 rel z-1">
-                        <div class="lp-form-bg">
-                            <img :src="formBg" alt="Hero Form">
-                        </div>
-                    </div>
                 </div>
-            </div>
-        </section>
-        <section class="section-two bgp-bottom bgp-bottom bgc-black py-100 rpt-95 rpb-100"
-            style="background-color: #fff;">
-            <div class="container">
-                <div class="row justify-content-center text-center">
-                    <div class="col-xl-8">
-                        <div class="section-title mb-50">
-                            <h2>Garanta seu lugar no evento de lançamento <span>gratuito</span></h2>
-                        </div>
-                        <div class="lp-form">
-                            <form @submit.prevent="submitForm">
-                                <div class="form-group">
-                                    <input type="text" id="name" v-model="name" required class="form-control" value=""
-                                        placeholder="Seu nome">
-                                </div>
-                                <div class="form-group">
-                                    <input type="text" id="name" v-model="email" class="form-control" value=""
-                                        placeholder="Seu e-mail" required>
-                                </div>
-                                <div class="form-group">
-                                    <input type="text" id="name" v-model="phone" class="form-control" value=""
-                                        placeholder="Seu whatsapp" required>
-                                </div>
-                                <div class="form-group py-30 mb-0">
-                                    <button v-if="!isLoading" type="submit" class="theme-btn">QUERO PARTICIPAR <i class="fas fa-solid fa-arrow-right"></i></button>
-                                    <div v-else class="loader">Enviando...</div>
-                                </div>
-                            </form>
-                            <div v-if="successMessage" class="success-message" v-html="successMessage"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            
+              </div>
         </section>
 
         <footer class="main-footer">
             <div class="container">
-                <div class="footer-bottom py-30">
+                <div class="footer-bottom">
                     <div class="row align-items-center">
                         <div class="col-xl-12 col-lg-12">
                             <div class="copyright-text pt-10 text-lg-start text-center">
-                                <p>Copyright 2024. Todos os direitos reservados</p>
+                                <p style="color:grey">Copyright 2024. Todos os direitos reservados</p>
                             </div>
                         </div>
                     </div>
@@ -93,7 +85,7 @@ export default {
             phone: '',
             successMessage: '',
             isLoading: false,
-            formBg: require('@/assets/images/form-bg.png'),
+            formBg: require('@/assets/images/IMG_6169.png'),
             backgroundImage: require('@/assets/images/lp-two.png'),
         };
     },
