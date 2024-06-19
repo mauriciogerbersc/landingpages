44a3547baf32acbcc4ffdbef96df81ecebcf181b
mauricio3332406 <mauricio@greenn.com.br>
2024-06-18 22:57:14 -0300

    alteracoes de layout

--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -1,77 +1,42 @@
 <template>
-   <body class="lp">
-    <div class="page-wrapper">
-        <section class="one-page bgp-bottom bgp-bottom bgc-black py-50 rpt-95 rpb-100" :style="backgroundImageStyle">
-            <div class="container">
-                <div class="row gap-150 justify-content-center">
-                    <div class="lp-content style-two text-white">
-                        <h1>Venha trilhar comigo a jornada do <span>seu crescimento</span></h1>
-                    </div>
-
-                    <div class="col-lg-6 ">
-                        <div class="lp-form-bg">
-                            <img :src="formBg">
-                        </div>
-                    </div>
-           
-                    <div class="col-lg-6 rel z-1">
-                        <div class="lp-content style-two text-white">
-                            <p>
-                                Minha história no mundo do artesanato começou em 2018, eu tive que aprender tudo do zero, desde as técnicas de artesanato até a construção de um negócio de sucesso nas redes sociais. 
-                            </p>
-                            <p>Consegui me destacar nesse mercado e criar duas empresas do ramo, alcançando todos os estados brasileiros e mais de 30 países.</p>
-                            <p>Agora vou ajudar você a construir um caminho de sucesso. Se você quer transformar sua paixão pelo artesanato em um negócio próspero, eu te convido a participar do nosso evento de lançamento gratuito!</p>
-                  
-                            <div class="lp-content style-two text-white">
-                                <h4><span>Garanta seu lugar no evento de lançamento gratuito:</span></h4>
-                            </div>
-
-                            <div class="lp-form">
-
-                                <form @submit.prevent="submitForm">
-                                    <div class="form-group">
-                                        <input type="text" id="name" v-model="name" required class="form-control" value=""
-                                            placeholder="Seu nome">
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="text" id="name" v-model="email" class="form-control" value=""
-                                            placeholder="Seu e-mail" required>
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="text" id="name" v-model="phone" class="form-control" value=""
-                                            placeholder="Seu whatsapp" required>
-                                    </div>
-                                    <div class="form-group py-30 mb-0">
-                                        <button v-if="!isLoading" type="submit" class="theme-btn">QUERO PARTICIPAR <i class="fas fa-solid fa-arrow-right"></i></button>
-                                        <div v-else class="loader">Enviando...</div>
-                                    </div>
-                                </form>
-                                <div v-if="successMessage" class="success-message" v-html="successMessage"></div>
-                            </div>
-
-                        </div>
-                    </div>
+    <div class="banner">
+        <div class="container">
+            <div class="left">
+                <header>
+                    <img :src="logo" alt="Logo" class="logo">
+                </header>
+                <h1>Venha trilhar comigo <span>a Jornada do Seu Crescimento</span></h1>
+                <p class="text-center">Minha história no mundo do artesanato começou em 2018, eu tive que aprender tudo
+                    do zero como
+                    construir um negócio de sucesso nas redes sociais. Agora quero te ensinar tudo que aprendi na minha
+                    trajetória!</p>
+                <div class="section-title">
+                    <h2>Inscreva-se e garanta sua vaga gratuita no evento de lançamento!
+                        Dia 30/06 às 18h Online e AO VIVO</h2>
                 </div>
-            
-              </div>
-        </section>
-
-        <footer class="main-footer">
-            <div class="container">
-                <div class="footer-bottom">
-                    <div class="row align-items-center">
-                        <div class="col-xl-12 col-lg-12">
-                            <div class="copyright-text pt-10 text-lg-start text-center">
-                                <p style="color:grey">Copyright 2024. Todos os direitos reservados</p>
-                            </div>
+                <div class="lp-form">
+                    <form>
+                        <div class="form-group">
+                            <input type="text" id="name" name="name" class="form-control" value=""
+                                placeholder="Seu nome" required="">
                         </div>
-                    </div>
+                        <div class="form-group">
+                            <input type="text" id="name" name="name" class="form-control" value=""
+                                placeholder="Seu e-mail" required="">
+                        </div>
+                        <div class="form-group text-center">
+                            <button v-if="!isLoading"  type="submit" class="theme-btn">QUERO PARTICIPAR</button>
+                            <div v-else class="loader">Enviando...</div>
+                        </div>
+                    </form>
+                    <div v-if="successMessage" class="success-message" v-html="successMessage"></div>
                 </div>
             </div>
-        </footer>
-
+            <div class="right">
+                <img :src="formBg">
+            </div>
+        </div>
     </div>
-</body>
 </template>
 
 <script>
@@ -85,8 +50,9 @@ export default {
             phone: '',
             successMessage: '',
             isLoading: false,
-            formBg: require('@/assets/images/IMG_6169.png'),
+            formBg: require('@/assets/images/form-bg.png'),
             backgroundImage: require('@/assets/images/lp-two.png'),
+            logo: require('@/assets/images/AD_horizontal 4.png'),
         };
     },
     computed: {
--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import App from './App.vue'
 
 import './assets/css/bootstrap.min.css';
 import './assets/css/fontawesome.min.css';
-import './assets/css/style.css';
+import './assets/css/styles.css';
 
 createApp(App).mount('#app')
